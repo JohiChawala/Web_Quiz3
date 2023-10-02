6ab5f91efd3ac534bcd2ad0398631215ed4afb66
JohiChawala <136928760+JohiChawala@users.noreply.github.com>
2023-10-02 15:13:20 +0500

    js-file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,15 @@
 // JavaScript code to handle form submission and validation
-           // JavaScript code to handle form submission and validation
-           document.getElementById("signup-button").addEventListener("click", function(event) {
-            const email = document.getElementById("email").value;
-            const password = document.getElementById("password").value;
-            const checkbox = document.querySelector("input[type='checkbox']");
-    
-            if (!email || !password) {
-                alert("Email and password are required.");
-                event.preventDefault(); // Prevent the form from being submitted
-            } else if (!checkbox.checked) {
-                alert("Please agree to the terms.");
-                event.preventDefault(); // Prevent the form from being submitted
-            } else {
-                alert("Congratulations! Signup successful.");
-                // You can remove the alert and submit the form or perform other actions here
-            }
-        });
+document.getElementById("signup-button").addEventListener("click", function(event) {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const checkbox = document.querySelector("input[type='checkbox']");
+
+    if (!email || !password || !checkbox.checked) {
+        alert("Please fill in all fields and agree to the terms.");
+        event.preventDefault(); // Prevent the form from being submitted
+    } 
+    else {
+        alert("Congratulations! Signup successful.");
+        // You can remove the alert and submit the form or perform other actions here
+    }
+});
